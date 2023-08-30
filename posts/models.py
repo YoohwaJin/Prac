78730a551620ab199b36faa98c6a36c60b8abc3e
YoohwaJin <qudghkehdtod@naver.com>
2023-08-30 18:19:31 +0900

    post 기능 구현

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
+from django_resized import ResizedImageField
 # Create your models here.
 class Post(models.Model):
     context = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='image/%Y/%m')
+    image = ResizedImageField(
+        size=[500, 500],
+        crop=['middle', 'center'],
+        upload_to='image/%Y/%m'
+    )
